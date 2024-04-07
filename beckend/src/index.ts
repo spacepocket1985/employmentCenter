f216be9d55c712d5fc25404f4362b5771e3ba9fb
Space Pocket <space1985@pocket@gmail.com>
2024-04-07 23:47:48 +0300

    feat: start passport configuration

--- beckend/src/index.ts
+++ beckend/src/index.ts
@@ -1,6 +1,8 @@
 import "express-async-errors";
 import express from "express";
 import cors from "cors";
+import passport from "passport";
+
 import connectDB from "./config/db.config";
 import vacancyRouter from "./routes/vacancy.routes";
 import authRouter from './routes/auth.routes'
@@ -10,6 +12,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(passport.initialize())
 
 const port = process.env.PORT || 5000;
 
---
+++ beckend/src/middleware/passport.ts
@@ -0,0 +1,1 @@
+import { Strategy, ExtractJwt } from "passport-jwt";
