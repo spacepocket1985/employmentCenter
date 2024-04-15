07eceec95936825960ac4ac391201a9400a58232
Space Pocket <space1985pocket@gmail.com>
2024-04-15 08:36:33 +0300

    refactor: remove advantagesData from dataSlice

--- frontend/src/components/advantages/Advantages.tsx
+++ frontend/src/components/advantages/Advantages.tsx
@@ -6,11 +6,43 @@ import {
   ListItemText,
 } from '@mui/material';
 
-import { useAppSelector } from '../../hooks/storeHooks';
+import PaidRoundedIcon from '@mui/icons-material/PaidRounded';
+import SavingsRoundedIcon from '@mui/icons-material/SavingsRounded';
+import SchoolRoundedIcon from '@mui/icons-material/SchoolRounded';
+import LocalHospitalRoundedIcon from '@mui/icons-material/LocalHospitalRounded';
+import HolidayVillageRoundedIcon from '@mui/icons-material/HolidayVillageRounded';
+import RestaurantMenuRoundedIcon from '@mui/icons-material/RestaurantMenuRounded';
+import PeopleRoundedIcon from '@mui/icons-material/PeopleRounded';
+
 import { UIModal } from '../ui/UIModal';
 
+const advantagesData = [
+  { title: 'Достойная заработная плата', icon: PaidRoundedIcon },
+  {
+    title: 'Дополнительные выплаты к государственным праздникам',
+    icon: SavingsRoundedIcon,
+  },
+  {
+    title: 'Возможность обучения и профессионального развития',
+    icon: SchoolRoundedIcon,
+  },
+  {
+    title: 'Наличие медицинского страхования',
+    icon: LocalHospitalRoundedIcon,
+  },
+  {
+    title: 'Возможность льготного оздоровления в санатории «Энергетик»',
+    icon: HolidayVillageRoundedIcon,
+  },
+  { title: 'Наличие столовй и буфета', icon: RestaurantMenuRoundedIcon },
+  {
+    title: 'Дружелюбная и поддерживающая рабочая атмосфера',
+    icon: PeopleRoundedIcon,
+  },
+];
+
 export const Advantages = () => {
-  const advantagesData = useAppSelector((state) => state.data.advantages);
+  
 
   const renderItems = advantagesData.map((item, index) => {
     return (
