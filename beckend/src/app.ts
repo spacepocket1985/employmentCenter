8278cf1c00497a9a3f867c3dc438807ec304ccf5
Space Pocket <space1985pocket@gmail.com>
2026-01-11 12:23:31 +0300

    add temp option for cors

--- beckend/src/app.ts
+++ beckend/src/app.ts
@@ -12,7 +12,12 @@ import { menuRoutes } from './routes/menu.routes';
 
 export const app = express();
 
-app.use(cors());
+const corsOptions = {
+  origin: 'http://localhost:5173',
+  credentials: true
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(passport.initialize());
